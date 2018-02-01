b856af6cd4cfb1b4363872bb1394bfa47e56d42b
erfan <eshratif@usc.edu>
2018-01-31 17:15:50 -0800

    - Homepage uploaded!

---
+++ js/cufon-replace.js
@@ -0,0 +1,2 @@
+Cufon.replace('.title2,h1,h2,h4,h5,.footer-menu li a,.button',{fontFamily:'Bebas Neue',hover:true})
+Cufon.replace('.sf-menu li li a:not(.sf-menu li li li a)',{fontFamily:'Bebas Neue',hover:true})
